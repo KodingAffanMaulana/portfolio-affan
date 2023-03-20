eabca3aa8bdcc153a7d61b8cac3cbcd0a1c9b8e9
KodingAffanMaulana <affan.maulana2016@students.unila.ac.id>
2023-03-20 15:18:18 +0700

    add: pageTransition

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -16,13 +16,14 @@ export default defineNuxtConfig({
         // { rel: 'icon', type: 'image/png', href: '~/affan.png' },
         { rel: "stylesheet", href: "https://cdn.jsdelivr.net/gh/devicons/devicon@v2.15.1/devicon.min.css" },
         { rel: "stylesheet", href: "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" },
+        { rel: 'stylesheet', href: 'https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css' },
         { rel: "preconnect", href: "https://fonts.googleapis.com" },
         { rel: "preconnect", href: "https://fonts.gstatic.com" },
         { href: "https://fonts.googleapis.com/css2?family=Inter:wght@400;700&display=swap", rel:"stylesheet"}
         // { rel: "stylesheet", href: "https://unpkg.com/aos@next/dist/aos.css"}
       ]
     },
-    pageTransition: { name: 'page', mode: 'in-out' },
+    pageTransition: { name: 'page', mode: 'out-in' },
   },
   plugins: [
     { src: '~/plugins/aos', mode: 'client' },
