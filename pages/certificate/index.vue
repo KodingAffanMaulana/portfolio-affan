045153289516bed7080b830da8ee42cbb7bd8367
KodingAffanMaulana <affan.maulana2016@students.unila.ac.id>
2023-03-20 15:19:23 +0700

    add: fixed error

--- pages/certificate/index.vue
+++ pages/certificate/index.vue
@@ -1,45 +1,32 @@
 <template>
-  <main
-    class="main bg-gradient-to-tr from-[gray-900] to-[#00ffcc] p-4 pt-[4.5rem] sm:p-8 sm:pt-[5.5rem]"
-  >
-    <div class="drop-shadow-2xl">
-      <section id="education" class="p-8 lg:px-[5rem] lg:pb-16 bg-white">
-        <div class="flex flex-col items-center justify-center text-center">
-          <h1
-            class="font-bold text-[16px] sm:text-2xl text-blue-900 p-2 sm:p-3 sm:pb-4"
-          >
-            Certificate
-          </h1>
-          <br />
-        </div>
-        <div class="bg-blue-50 p-2 lg:p-4 rounded-[15px]">
-          <div
-            class="grid-cols-1 sm:grid-cols-1 md:grid-cols-1 lg:grid-cols-3 grid gap-6"
-          >
-            <article
-              v-for="(sertif, index) in sertifs"
-              :key="index"
-              class="bg-gray-800 shadow-xl rounded-[15px] h-[auto] hover:transform hover:translate-x-4 hover:translate-y-4 hover:shadow-blue-400 hover:"
-            >
-              <img
-                class="w-[100%] object-contain rounded-t-[15px]"
-                :src="sertif.src"
-                alt="sertifikat1"
-              />
-              <div class="p-2 sm:p-5">
-                <h2
-                  class="font-bold text-[13px] sm:text-xl text-teal-100 pb-1 text-center"
-                >
-                  {{ sertif.title }}
-                </h2>
-              </div>
-            </article>
+  <div>
+    <main class="main bg-gradient-to-tr from-[gray-900] to-[#00ffcc] p-4 pt-[4.5rem] sm:p-8 sm:pt-[5.5rem]">
+      <div class="drop-shadow-2xl">
+        <section id="education" class="p-8 lg:px-[5rem] lg:pb-16 bg-white">
+          <div class="flex flex-col items-center justify-center text-center">
+            <h1 class="font-bold text-[16px] sm:text-2xl text-blue-900 p-2 sm:p-3 sm:pb-4">
+              Certificate
+            </h1>
+            <br />
           </div>
-        </div>
-      </section>
-    </div>
-  </main>
-  <Footer />
+          <div class="bg-blue-50 p-2 lg:p-4 rounded-[15px]">
+            <div class="grid-cols-1 sm:grid-cols-1 md:grid-cols-1 lg:grid-cols-3 grid gap-6">
+              <article v-for="(sertif, index) in sertifs" :key="index"
+                class="bg-gray-800 shadow-xl rounded-[15px] h-[auto] hover:transform hover:translate-x-4 hover:translate-y-4 hover:shadow-blue-400 hover:">
+                <img class="w-[100%] object-contain rounded-t-[15px]" :src="sertif.src" alt="sertifikat1" />
+                <div class="p-2 sm:p-5">
+                  <h2 class="font-bold text-[13px] sm:text-xl text-teal-100 pb-1 text-center">
+                    {{ sertif.title }}
+                  </h2>
+                </div>
+              </article>
+            </div>
+          </div>
+        </section>
+      </div>
+    </main>
+    <Footer />
+  </div>
 </template>
 
 <script setup>
@@ -94,5 +81,3 @@ const sertifs = ref([
   },
 ]);
 </script>
-
-<style scoped></style>
--- pages/project/index.vue
+++ pages/project/index.vue
@@ -1,50 +1,38 @@
 <template>
-  <main
-    class="main bg-gradient-to-tr from-[gray-900] to-[#00ffcc] p-4 pt-[4.5rem] sm:p-8 sm:pt-[5.5rem]"
-  >
-    <div class="drop-shadow-2xl">
-      <section id="project" class="p-8 lg:px-[5rem] lg:pb-16 bg-white">
-        <div class="flex flex-col items-center justify-center text-center">
-          <h1
-            class="font-bold text-[16px] sm:text-2xl text-blue-900 p-2 sm:p-3 sm:pb-8"
-          >
-            Project
-          </h1>
-          <br />
-        </div>
-        <div class="grid lg:grid-cols-4 lg:gap-8 lg:pb-8">
-          <article
-            v-for="(project, index) in projects"
-            :key="index"
-            class="pb-4 lg:pb-8 shadow-xl hover:border-[0.1rem] hover:border-blue-500 rounded-xl hover:shadow-blue-200"
-            data-aos="flip-left" 
-          >
-            <div class="p-4">
-              <h2 class="font-bold text-[13px] sm:text-xl text-center pb-2">
-                {{ project.title }}
-              </h2>
-              <p class="text-[10px] text-sm pb-2 text-justify">
-                {{ project.desc }}
-              </p>
-              <img
-                :src="project.url"
-                alt="fanphotos"
-                class="object-fit cover rounded-xl pb-4"
-              />
-            </div>
-            <button
-              class="text-[10px] sm:text-[14px] flex mx-auto rounded-xl justify-beetween py-1 px-4 bg-blue-500 text-white hover:border-2 hover:border-blue-500 hover:bg-white hover:text-blue-500"
-            >
-              <a :href="project.link" target="_blank" rel="noopener noreferrer"
-                >Details</a
-              >
-            </button>
-          </article>
-        </div>
-      </section>
-    </div>
-  </main>
-  <Footer />
+  <div>
+    <main class="main bg-gradient-to-tr from-[gray-900] to-[#00ffcc] p-4 pt-[4.5rem] sm:p-8 sm:pt-[5.5rem]">
+      <div class="drop-shadow-2xl">
+        <section id="project" class="p-8 lg:px-[5rem] lg:pb-16 bg-white">
+          <div class="flex flex-col items-center justify-center text-center">
+            <h1 class="font-bold text-[16px] sm:text-2xl text-blue-900 p-2 sm:p-3 sm:pb-8">
+              Project
+            </h1>
+            <br />
+          </div>
+          <div class="grid lg:grid-cols-4 lg:gap-8 lg:pb-8">
+            <article v-for="(project, index) in projects" :key="index"
+              class="pb-4 lg:pb-8 shadow-xl hover:border-[0.1rem] hover:border-blue-500 rounded-xl hover:shadow-blue-200"
+              data-aos="flip-left">
+              <div class="p-4">
+                <h2 class="font-bold text-[13px] sm:text-xl text-center pb-2">
+                  {{ project.title }}
+                </h2>
+                <p class="text-[10px] text-sm pb-2 text-justify">
+                  {{ project.desc }}
+                </p>
+                <img :src="project.url" alt="fanphotos" class="object-fit cover rounded-xl pb-4" />
+              </div>
+              <button
+                class="text-[10px] sm:text-[14px] flex mx-auto rounded-xl justify-beetween py-1 px-4 bg-blue-500 text-white hover:border-2 hover:border-blue-500 hover:bg-white hover:text-blue-500">
+                <a :href="project.link" target="_blank" rel="noopener noreferrer">Details</a>
+              </button>
+            </article>
+          </div>
+        </section>
+      </div>
+    </main>
+    <Footer />
+  </div>
 </template>
 
 <script setup lang="ts">
