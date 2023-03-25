dbb5c8e6cac819eae9df9b8332ed53a0c07f2449
KodingAffanMaulana <affan.maulana2016@students.unila.ac.id>
2023-03-25 21:30:03 +0700

    add: fixed footer icon hover

--- components/Footer/Footer.vue
+++ components/Footer/Footer.vue
@@ -5,28 +5,28 @@
         <a
           href="https://github.com/KodingAffanMaulana"
           target="_blank"
-          class="fa fa-github hover:text-black transition duration-200 ease-in"
+          class="fa fa-github transition duration-100 transform hover:-translate-y-1 transition duration-200 ease-in"
         ></a>
       </ul>
       <li>
         <a
           href="https://www.instagram.com/affan_maulaana?r=nametagg"
           target="_blank"
-          class="fa fa-instagram hover:text-black transition duration-200 ease-in"
+          class="fa fa-instagram transition duration-100 transform hover:-translate-y-1 transition duration-200 ease-in"
         ></a>
       </li>
       <li>
         <a
           href="https://www.linkedin.com/in/affan-maulana-41a1a2194/"
           target="_blank"
-          class="fa fa-linkedin hover:text-black transition duration-200 ease-in"
+          class="fa fa-linkedin transition duration-100 transform hover:-translate-y-1 transition duration-200 ease-in"
         ></a>
       </li>
       <li>
         <a
           href="https://api.whatsapp.com/send?phone=6289619258610"
           target="_blank"
-          class="fa fa-whatsapp hover:text-black transition duration-200 ease-in"
+          class="fa fa-whatsapp transition duration-100 transform hover:-translate-y-1 transition duration-200 ease-in"
         ></a>
       </li>
     </ul>
