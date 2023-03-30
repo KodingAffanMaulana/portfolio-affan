a6d0952c681da643a14b79750513fe2dd0cc5471
KodingAffanMaulana <affan.maulana2016@students.unila.ac.id>
2023-03-30 14:33:40 +0700

    fixed: logo footer

--- components/Footer/Footer.vue
+++ components/Footer/Footer.vue
@@ -1,33 +1,21 @@
 <template>
   <footer class="shadow-lg bg-gray-900 text-xl p-4 mx-auto">
     <ul class="flex justify-center gap-8 text-white">
-      <ul>
-        <a
-          href="https://github.com/KodingAffanMaulana"
-          target="_blank"
-          class="fa fa-github transition duration-100 transform hover:-translate-y-1 transition duration-200 ease-in"
-        ></a>
-      </ul>
       <li>
-        <a
-          href="https://www.instagram.com/affan_maulaana?r=nametagg"
-          target="_blank"
-          class="fa fa-instagram transition duration-100 transform hover:-translate-y-1 transition duration-200 ease-in"
-        ></a>
+        <NuxtLink to="https://github.com/KodingAffanMaulana" target="_blank"
+          class="fa fa-github transition duration-100 transform hover:-translate-y-1 transition duration-200 ease-in" />
       </li>
       <li>
-        <a
-          href="https://www.linkedin.com/in/affan-maulana-41a1a2194/"
-          target="_blank"
-          class="fa fa-linkedin transition duration-100 transform hover:-translate-y-1 transition duration-200 ease-in"
-        ></a>
+        <NuxtLink to="https://www.instagram.com/affan_maulaana?r=nametagg" target="_blank"
+          class="fa fa-instagram transition duration-100 transform hover:-translate-y-1 transition duration-200 ease-in" />
       </li>
       <li>
-        <a
-          href="https://api.whatsapp.com/send?phone=6289619258610"
-          target="_blank"
-          class="fa fa-whatsapp transition duration-100 transform hover:-translate-y-1 transition duration-200 ease-in"
-        ></a>
+        <NuxtLink to="https://www.linkedin.com/in/affan-maulana-41a1a2194/" target="_blank"
+          class="fa fa-linkedin transition duration-100 transform hover:-translate-y-1 transition duration-200 ease-in" />
+      </li>
+      <li>
+        <NuxtLink to="https://api.whatsapp.com/send?phone=6289619258610" target="_blank"
+          class="fa fa-whatsapp transition duration-100 transform hover:-translate-y-1 transition duration-200 ease-in" />
       </li>
     </ul>
 
@@ -40,7 +28,8 @@
 <script setup></script>
 
 <style>
-html, body {
+html,
+body {
   overflow-x: hidden;
 }
 </style>
